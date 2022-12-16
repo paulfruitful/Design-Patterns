6ad3753f76d9dfe6236b446e2f97dfaa4b0a9811
Paul Fruitful <fruitful2007@outlook.com>
2022-12-16 20:08:58 +0100

    The Constructor Design Pattern

--- constructor.js
+++ constructor.js
@@ -1,1 +1,18 @@
 //The Constructor Design Pattern
+class Child{
+    
+    //Definition of the object structure
+    constructor(name,age){
+        this.name=name
+        this.age=age
+    }
+    intro(){
+        return `I am ${this.name} and I am ${this.age} years old`
+    }
+}
+
+//Instantiation of the object
+const Jack=new Child('Jack',12)
+
+//Returns "I am Jack and I am 12 years old"
+console.log(Jack.intro()) 
